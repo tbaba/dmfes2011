0520bfa3fecc05644dad833dc6873c757b84b88e
Tatsuro Baba <harakirisoul@gmail.com>
2011-07-30 18:34:25 +0900

    colorize only current tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+
+  def current_tab(name)
+    name == current_tab_name ? "current #{name}" : name
+  end
+
+  def current_tab_name
+    c, a = controller_name, action_name
+
+    case [c, a]
+    when ['welcome', 'index']
+      'top'
+    when ['welcome', 'about']
+      'about'
+    when ['welcome', 'access']
+      'access'
+    when ['welcome', 'news']
+      'news'
+    when ['articles', a]
+      'news'
+    when ['welcome', 'artists']
+      'artists'
+    end
+  end
 end
