9f33bcb9152ad94648145010f335a54a9ab85790
馬場 達郎 <harakirisoul@baba-no-MacBook-Air.local>
2011-07-30 09:32:28 +0900

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dmfes::Application.config.secret_token = 'c9065586adc1cb642119b514c144d6c6c68e5fd5ee4790e23b725c32df4275a40e705f9455a0712212f9f0c08168bd80824816c44d34428052c4815b212ba86b'
