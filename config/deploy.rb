951e1c02b22f655be5b037fef1982ffebc3126c7
Tatsuro Baba <harakirisoul@gmail.com>
2011-07-31 18:34:29 +0900

    deployment configurations

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,13 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "dmfes"
+set :repository,  "git@github.com:harakirisoul/dmfes2011.git"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :git_shallow_clone, 1
+default_run_options[:pty] = true
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :use_sudo, false
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :scm, :git
+set :user, 'tatsuro'
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+set :deploy_to, "/var/www/#{application}"
+server '59.106.183.24', :app, :web, :db, :primary => true
